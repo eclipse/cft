624d0e4f1e852522d10579ae7bacbf8e0c3a24d8
Jonathan West <jgwest@ca.ibm.com>
2016-04-05 18:36:50 -0400

    [491122] - RefreshEditorAction temporarily blocks the UI thread to refresh CF server domains, which may occur on workbench startup

--- org.eclipse.cft.server.ui/src/org/eclipse/cft/server/ui/internal/actions/RefreshEditorAction.java
+++ org.eclipse.cft.server.ui/src/org/eclipse/cft/server/ui/internal/actions/RefreshEditorAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2016 Pivotal Software, Inc. and IBM Corporation.
+ * Copyright (c) 2012, 2016 Pivotal Software, Inc. 
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
