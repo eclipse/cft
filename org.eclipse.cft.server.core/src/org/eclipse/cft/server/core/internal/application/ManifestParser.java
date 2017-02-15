f39c334c9aa92bf9e6b75d030bb4312ffb771697
eyuen <eyuen@ca.ibm.com>
2017-02-15 17:31:17 -0500

    [509481] Empty manifest.yml does not allow saving of new manifest

--- org.eclipse.cft.server.core/src/org/eclipse/cft/server/core/internal/application/ManifestParser.java
+++ org.eclipse.cft.server.core/src/org/eclipse/cft/server/core/internal/application/ManifestParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2016 Pivotal Software, Inc. and others
+ * Copyright (c) 2013, 2017 Pivotal Software, Inc. and others
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -17,6 +17,7 @@
  *  
  *  Contributors:
  *     Pivotal Software, Inc. - initial API and implementation
+ *     IBM - bug fix
  ********************************************************************************/
 package org.eclipse.cft.server.core.internal.application;
 
@@ -622,7 +623,13 @@ public class ManifestParser {
 
 			String appName = deploymentInfo.getDeploymentName();
 
-			Map<Object, Object> deploymentInfoYaml = parseManifestFromFile();
+			Map<Object, Object> deploymentInfoYaml = null;
+			try {
+				deploymentInfoYaml = parseManifestFromFile();
+			} catch (CoreException e) {
+				// Failed to load the original manifest file. Continue to try to write the new content.
+				CloudFoundryPlugin.logWarning(e.getMessage());
+			}
 
 			subProgress.worked(1);
 
